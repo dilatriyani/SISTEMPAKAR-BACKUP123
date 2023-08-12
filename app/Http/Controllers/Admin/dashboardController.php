df245b8c1c3d66e7b8ac5e43fdd5c3b097521e39
ameliarnt <amelia@mplus.software>
2023-08-12 21:34:20 +0700

    score counting changes

--- app/Http/Controllers/Admin/dashboardController.php
+++ app/Http/Controllers/Admin/dashboardController.php
@@ -58,14 +58,9 @@ class dashboardController extends Controller
         ]);
     }
 
-    public function result(Request $request,)
+    public function result(Request $request)
     {
-        $rule_last = Rule::latest()->first();
-        $penyakit_last = data_penyakit::latest()->first();
         $last = Gejala::latest()->first();
-        if ($last == null || $penyakit_last == null || $rule_last == null) {
-            return redirect('')->with('error', 'Inform admin, data is not updated');
-        }
         $penyakit_total = data_penyakit::get()->count();
         $id_gejala = $request->id_gejala;
         $next = $id_gejala + 1;
@@ -77,6 +72,7 @@ class dashboardController extends Controller
                 if ($request->answer == 1) {
                     # save gejala selected
                     Session::push('gejala', $gejala->nama_gejala);
+                    Session::push('IdGejala', $gejala->id);
                     for ($i = 1; $i <= $penyakit_total; $i++) {
                         $rule = Rule::query()->findOrFail($i);
                         $daftar_gejala = explode(',', $rule->daftar_gejala);
@@ -98,14 +94,49 @@ class dashboardController extends Controller
                 return redirect('/Pengguna/Diagnosa/1')->with(['message' => 'Pilih setidaknya 1 Gejala!']);
             }
 
-            $countArray = array_count_values($penyakit);
-            $arrayLength = count($penyakit);
-            arsort($countArray);
-            $mostFrequentValue = key($countArray);
-            $frequency = current($countArray);
-            $score = $frequency / $arrayLength * 100;
+            $id_gejala_all = Session::get('IdGejala');
+            $id_penyakit_possible = array_unique(Session::get('penyakit'));
+            $score_results = [];
+            
+            // perhitungan score
+            foreach ($id_penyakit_possible as $penyakit_id) {
+                $rules_for_penyakit = Rule::where('id_penyakit', $penyakit_id)->get();
+                $counter = 0;
+                foreach ($rules_for_penyakit as $rule) {
+                    $gejala_list_for_rule = explode(',', $rule->daftar_gejala);
+            
+                    foreach ($gejala_list_for_rule as $gejala_id) {
+                        foreach ($id_gejala_all as $applied_gejala) {
+                            if ($gejala_id == $applied_gejala) {
+                                $counter++;
+                            }
+                        }
+                    }
+            
+                    $score = $counter / count($gejala_list_for_rule) * 100;
+                    $score_results[] = $score;
+                }
+            }
 
-            $penyakit_result = data_penyakit::query()->findOrFail($mostFrequentValue);
+            Session::put('score', $score_results);
+            
+            // dd($id_gejala_all,$id_penyakit_possible, Session::get('score'));
+
+            // kemungkianan dan score ada diarrray berbeda dengan urutan sama
+            // ambil index untuk kemungkinan tertinggi
+            $score_all = Session::get('score');
+            $max_score = max($score_all);
+            $maxIndex = array_search($max_score, $score_all); 
+            $max_possible_penyakit = $id_penyakit_possible[$maxIndex];
+            
+            // $countArray = array_count_values($penyakit);
+            // $arrayLength = count($penyakit);
+            // arsort($countArray);
+            // $mostFrequentValue = key($countArray);
+            // $frequency = current($countArray);
+            // $score = $frequency / $arrayLength * 100;
+
+            $penyakit_result = data_penyakit::query()->findOrFail($max_possible_penyakit);
 
             $user_name = Session::get('user_name');
             $user_age = Session::get('user_age');
@@ -117,7 +148,7 @@ class dashboardController extends Controller
                 'umur' => last($user_age),
                 'alamat' => last($user_address),
                 'penyakit' => $penyakit_result->nama_penyakit,
-                'persentase' => strval($score),
+                'persentase' => strval($max_score),
                 'solusi' => $penyakit_result->solusi,
             ]);
 
